fcc6fb722ae2bc5ab9e21776e92eeb38406e3985
Charlie Root <josiaslg@nt.inf.br>
2022-10-15 15:12:06 +0000

    Update lists

---
+++ GenerateAlienVaultListToRouterOS.bash
@@ -0,0 +1,45 @@
+#!/usr/local/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin
+
+# The objective is download list from Alien Vault and
+# prepare to be used on RouterOS for basic protection.
+# A script on RouterOS will refresh every day, downloading this modified file.
+# Execute this script on FreeBSD/BSD* servers. For Linux just change the top to /bin/bash.
+# 
+
+# Version: 13 octuber 2022v1
+# Initial to generate rules
+
+# Created by josiaslg@bsd.com.br / @josiaslg
+
+# Interface to Internet
+# Change to yours. This is who connect by pppoe. 
+# If you use static IP, DHCP or other, edit with the correct interface
+IF_INTERNET=pppoe-out1
+
+# Folder Temp
+FOLDER_TEMP=/tmp
+
+# Level Alien Vault
+NETWORKS_TO_BLOCK_1=alienvault_reputation.ipset
+NETWORKS_TO_BLOCK_ROUTEROS_1=RouterOS_AlienVault_$NETWORKS_TO_BLOCK_1
+
+# Download lists
+wget https://iplists.firehol.org/files/$NETWORKS_TO_BLOCK_1 -O $FOLDER_TEMP/$NETWORKS_TO_BLOCK_1
+
+# List 1 processing
+file="$FOLDER_TEMP/$NETWORKS_TO_BLOCK_1"
+while read -r line; do
+    [[ "$line" =~ ^#.*$ ]] && continue
+    echo "/ip firewall filter add chain=input src-address=${line} in-interface=$IF_INTERNET action=drop  comment "$NETWORKS_TO_BLOCK_ROUTEROS_1"" >> $FOLDER_TEMP/$NETWORKS_TO_BLOCK_ROUTEROS_1
+    echo "/ip firewall filter add chain=output dst-address=${line} out-interface=$IF_INTERNET action=drop comment "$NETWORKS_TO_BLOCK_ROUTEROS_1"" >> $FOLDER_TEMP/$NETWORKS_TO_BLOCK_ROUTEROS_1
+done < "$FOLDER_TEMP/$NETWORKS_TO_BLOCK_1"
+
+# Removing original file
+rm $FOLDER_TEMP/$NETWORKS_TO_BLOCK_1
+
+cd /home/suporte/RouterOS-iplists.firehol.org
+mv $FOLDER_TEMP/$NETWORKS_TO_BLOCK_ROUTEROS_1 ./
+git add -A
+git commit -a  -m 'Update lists'
+git push
